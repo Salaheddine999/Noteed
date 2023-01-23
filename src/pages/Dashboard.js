d173ba0f528b9645b8393a15fc91660aec1cb623
Salah eddine Ab <abdelwahid.salaheddine@gmail.com>
2023-01-23 04:09:25 +0100

    add sorting

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,33 +3,59 @@ import { Link } from "react-router-dom";
 import NoteList from "../components/notes/NoteList";
 import Spinner from "../components/spinner/Spinner";
 import { useNotesData } from "../hooks/useNote";
+import { useState } from "react";
 
 const Dashboard = () => {
 
-
+  const [sortOrder, setSortOrder] = useState('asc');
   const { user, isAuthenticated } = useAuth0();
   const { email } = user
 
   const {data, isFetching} = useNotesData(email)
 
-
+  const sortedData = data?.data.sort((a, b) => {
+    if (sortOrder === 'asc') {
+        return new Date(a.created_at) - new Date(b.created_at)
+    } else {
+      return new Date(b.created_at) - new Date(a.created_at)
+    }
+  })
 
   return (
     isAuthenticated && (
       <div className="mx-8 lg:mx-8 md:mx-4 sm:mx-2">
-        <div className="grid grid-cols-2 mb-20 mt-20">
+        <div className="grid grid-cols-2 mb-10 mt-20">
           <h2 className="text-5xl font-semibold lg:text-5xl md:text-xl sm:text-xl">My notes</h2>
-          <Link className="btn btn-secondary sm:btn-md md:btn-md lg:btn-md col-end-7" to={"/add-note"}>New note</Link>
         </div>
+
+        <div className="navbar mb-14">
+          <div className="flex-1">
+            <p className="font-semibold text-lg">Notes list</p>
+          </div>
+          <div className="flex-none">  
+            <select className="select select-ghost select-md mr-3">
+              <option selected disabled>Sort by</option>
+              <option onClick={()=>setSortOrder('asc')}>New</option>
+              <option onClick={()=>setSortOrder('desc')}>Old</option>
+            </select>
+            <Link className="btn gap-2" to={"/add-note"}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4.5v15m7.5-7.5h-15" />
+              </svg>
+               New
+            </Link>
+          </div>
+        </div>
+
         {isFetching ? 
           <div className="grid justify-items-center">
               <Spinner/>
           </div>
         :
-          ( data?.data.length > 0?
+          ( sortedData?.length > 0?
             (
             <div className="grid grid-cols-1 gap-8 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1">
-              {data?.data.map(note=>(
+              {sortedData?.map(note=>(
                   <NoteList key={note.id} note={note}/>
               ))}
             </div>
