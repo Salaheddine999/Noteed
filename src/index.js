0e15cd7fa5c7c60a96b54a69690d7c0c47c1c5e2
salah eddine <abdelwahid.salaheddine@gmail.com>
2024-03-06 18:50:54 +0100

    add analytics

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Auth0ProviderWithNavigate from './auth/auth0Provider';
+import { Analytics } from "@vercel/analytics/react";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <App />
-  
+    <>
+        <Analytics/>
+        <App />
+    </>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
